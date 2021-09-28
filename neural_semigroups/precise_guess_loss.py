f45347949da12fe06b17d506edf809bde1f8a07b
zarebulic <zare.bulic2@gmail.com>
2021-09-28 15:00:39 +0200

    Update precise_guess_loss.py

--- neural_semigroups/precise_guess_loss.py
+++ neural_semigroups/precise_guess_loss.py
@@ -16,7 +16,7 @@
 from torch import Tensor
 from torch.nn import Module
 
-from utils import count_different, make_discrete
+from neural_semigroups.utils import count_different, make_discrete
 
 
 class PreciseGuessLoss(Module):
